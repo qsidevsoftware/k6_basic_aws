511317f0f16f8756e1eeff0eb983abb76e842244
Marcelo Martins <qsidevsoftware@gmail.com>
2024-04-05 21:31:43 -0300

    -- ocrrecao test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,25 +1,20 @@
-
-//Escopo
 import http from 'k6/http';
 import { check } from 'k6';
 
-//Configuração
 export const options = {
     vus: 100,
     duration: '1m',
     thresholds: {
-        http_req_faild: ['rate < 0.05']
+        http_req_failed: ['rate < 0.05'],
     }
 }
 
-
-//Execução
-export default function () {
-    const BASE_URL = 'https://test-api.k6.io/public/crocodiles';
+export default function(){
+    const BASE_URL = 'https://test-api.k6.io/public/crocodiles/';
 
     const res = http.get(BASE_URL);
 
     check(res, {
-        'is status code 200': (r) => r.status === 200
+        'status code 200': (r) => r.status === 200
     });
 }
